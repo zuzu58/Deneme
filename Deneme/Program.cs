3741a1bfea49574a6c020f1af23fd34a47e6b5b3
zuzu58 <omr.suna@gmail.com>
2015-02-04 16:11:43 +0200

    fsdf

--- Deneme/Program.cs
+++ Deneme/Program.cs
@@ -12,7 +12,7 @@ namespace Deneme
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {   
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
