2c0e7af8f5c40c8e4647a8b9edb604060b9c2162
zuzu58 <omr.suna@gmail.com>
2015-02-04 16:18:33 +0200

    dqwrwewer

--- Deneme/Program.cs
+++ Deneme/Program.cs
@@ -16,6 +16,8 @@ namespace Deneme
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+           
         }
     }
 }
